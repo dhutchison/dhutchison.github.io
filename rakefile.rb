f5c0a59683d012abf243f61ef8d8c3d49c6026de
Davidh <dhutchison86+github@googlemail.com>
2013-09-19 22:17:44 +0100

    Added publish task for publishing a draft

--- rakefile.rb
+++ rakefile.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'rubygems'
 require 'jekyll'
 require 'zemanta' # gem install zemanta
+require 'stringex' # gem install stringex
  
 def get_zemanta_terms(content)
   $stderr.puts "Querying Zemanta..."
@@ -200,4 +201,72 @@ def upload
     sh 'rsync -avz --delete ../dwi_built_site/ david@dev-lamp.local:/home/wwwroot/dwi/'
     puts 'Sent'
 
+end
+
+desc "Publish a draft"
+task :publish, :filename do |t, args|
+  # if there's a filename passed (rake publish[filename])
+  # use it. Otherwise, list all available drafts in a menu
+  
+  source_dir = '/Users/david/Sites/dhutchison.github.io'
+  posts_dir = '_posts'
+  new_post_ext = 'markdown'
+  
+  unless args.filename
+    file = choose_file(File.join(source_dir,'_drafts'))
+    Process.exit unless file # no file selected
+  else
+    file = args.filename if File.exists?(File.expand_path(args.filename))
+    raise "Specified file not found" unless file
+  end
+  now = Time.now
+  short_date = now.strftime("%Y-%m-%d")
+  long_date = now.strftime("%Y-%m-%d %H:%M")
+
+  # separate the YAML headers
+  contents = File.read(file).split(/^---\s*$/)
+  if contents.count < 3 # Expects the draft to be properly formatted
+    puts "Invalid header format on post #{File.basename(file)}"
+    Process.exit
+  end
+  # parse the YAML. So much better than regex search and replaces
+  headers = YAML::load("---\n"+contents[1])
+  content = contents[2].strip
+
+  should = { :generate => false, :deploy => false, :schedule => false, :limit => 0 }
+
+  ### draft publishing ###
+  # fall back to current date and title-based slug
+  headers['date'] ||= long_date
+  headers['slug'] ||= headers['title'].to_url.downcase
+
+  # write out the modified YAML and post contents back to the original file
+  File.open(file,'w+') {|file| file.puts YAML::dump(headers) + "---\n" + content + "\n"}
+  # move the file to the posts folder with a standardized filename
+  target = "#{source_dir}/#{posts_dir}/#{short_date}-#{headers['slug']}.#{new_post_ext}"
+  mv file, target
+  puts %Q{Published "#{headers['title']}" to #{target}}
+  # deploy the site, full build
+  Rake::Task[:build].invoke('full')
+end
+
+# Creates a user selection menu from directory listing
+def choose_file(dir)
+  puts "Choose file: #{dir}"
+  @files = Dir["#{dir}/*"]
+  @files.each_with_index { |f,i| puts "#{i+1}: #{f}" }
+  print "> "
+  num = STDIN.gets
+  return false if num =~ /^[a-z ]*$/i
+  file = @files[num.to_i - 1]
+end
+
+def ask(message, valid_options)
+  return true if $skipask
+  if valid_options
+    answer = get_stdin("#{message} #{valid_options.delete_if{|opt| opt == ''}.to_s.gsub(/"/, '').gsub(/, /,'/')} ") while !valid_options.map{|opt| opt.nil? ? '' : opt.upcase }.include?(answer.nil? ? answer : answer.upcase)
+  else
+    answer = get_stdin(message)
+  end
+  answer
 end
