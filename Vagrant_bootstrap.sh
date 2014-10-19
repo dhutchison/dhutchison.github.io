2f65642dd95b65d3d3129145c7b5b7d9d3b2deff
Davidh <dhutchison86+github@googlemail.com>
2014-10-19 22:52:42 +0100

    Disable SendFile to work around issue

--- Vagrant_bootstrap.sh
+++ Vagrant_bootstrap.sh
@@ -3,5 +3,6 @@
 yum install -y httpd.x86_64
 rm -rf /var/www/html
 ln -fs /vagrant_data /var/www/html
+echo "EnableSendfile off" >> /etc/httpd/conf/httpd.conf
 chkconfig httpd on
 service httpd start
